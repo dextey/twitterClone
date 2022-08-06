ec7f72a25d0b229b830209a8dea0b62b7105f30d
Dexter <dexterdevmode@gmail.com>
2022-08-06 17:06:34 +0530

    Added error handler

---
+++ server/middleware/errorMiddleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+  errorHandler: (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+    });
+
+    next();
+  },
+};
