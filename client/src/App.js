f4b21188d7019da4a45e558b31964bdbb09acd94
Dexter <dexterdevmode@gmail.com>
2022-08-06 15:41:48 +0530

    Initial setup of Twitter

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <header className="text-3xl font-bold underline">
+        <span>Just a twitter Clone</span>
+      </header>
+    </div>
+  );
+};
+
+export default App;
