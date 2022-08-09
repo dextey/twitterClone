47d2cab35a4740782cbd345f2b12a5b33c447b65
Dexter <dexterdevmode@gmail.com>
2022-08-09 22:29:44 +0530

    Adding Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import Content from "./Components/Content";
+import Sidebar from "./Components/Sidebar";
+import SidebarRight from "./Components/SidebarRight";
+
 const App = () => {
   return (
-    <div>
-      <header className="text-3xl font-bold underline">
-        <span>Just a twitter Clone</span>
-      </header>
+    <div className="container mx-auto">
+      <div className="flex  mx-10">
+        <div className="flex w-3/12">
+          <Sidebar />
+        </div>
+        <div className="flex flex-grow ">
+          <Content />
+        </div>
+        <div className="flex w-3/12">
+          <SidebarRight />
+        </div>
+      </div>
     </div>
   );
 };
